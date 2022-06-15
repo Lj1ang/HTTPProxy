7d5ade65ff3d831f5f292803ac49d17da4fa7ac7
Lj1ang <kwaizmark@gmail.com>
2022-06-15 15:26:08 +0800

    finish save pass file from server

--- Client.java
+++ Client.java
@@ -1,24 +1,20 @@
 import java.io.*;
 import java.net.Socket;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.nio.Buffer;
-import java.util.ArrayList;
+import java.net.SocketException;
 
 public class Client {
 
     private String proxy_ip;
     private int port;
     private String requested_host;
-    private String requested_url;
-    private String local_filename;
+    final private String requested_url;
+    private String local_url;
 
-    public Client(String proxy_ip, int port,String requested_host,String requested_url,String local_filename){
+    public Client(String proxy_ip, int port,String requested_host,String requested_url){
        this.proxy_ip=proxy_ip;
        this.port=port;
        this.requested_host=requested_host;
        this.requested_url = requested_url;
-       this.local_filename=local_filename;
     }
 
     public void work() {
@@ -30,12 +26,17 @@ public class Client {
             // PrintWriter(string) ----> OutputStreamWriter ----> OutputStream(bytes)  ||
             PrintWriter pw = new PrintWriter(new OutputStreamWriter(client_out));
 
-            System.out.println(compositeRequest(requested_host));
-            pw.write(compositeRequest(requested_host));
+            System.out.println(compositeRequest(requested_url,requested_host));
+            pw.write(compositeRequest(requested_url,requested_host));
             pw.flush();
 
+
+            String[] requested_url_array=requested_url.split("/");
+            local_url=requested_url_array[requested_url_array.length-1];
             InputStream client_in = client.getInputStream();
-            parseResponse(client_in,requested_url,local_filename);
+            parseResponse(client_in,local_url);
+
+
             System.out.println("save done");
 
         } catch (IOException e) {
@@ -44,73 +45,111 @@ public class Client {
     }
 
 
-    public static String compositeRequest(String request_host){
+    public static String compositeRequest(String requested_url,String requested_host){
 
-        return "GET / HTTP/1.1\r\n" +
-                "Host: " + request_host + "\r\n" +
+        return "GET "+ requested_url +" HTTP/1.1\r\n" +
+                "Host: " + requested_host + "\r\n" +
                 "User-Agent: curl/7.43.0\r\n" +
                 "Accept: */*\r\n\r\n";
     }
 
-    public static void parseResponse(InputStream in, String requested_url,String local_filename) throws IOException {
+    public static void parseResponse(InputStream in, String local_url) throws IOException {
+        System.out.println("start parse response header");
         StringBuilder header=new StringBuilder();
-
-        String each_line="";
+        String each_line;
         LineBuffer line_buffer = new LineBuffer(3000);
-        int flag=0;
+
         while(null != (each_line = line_buffer.readLine(in) )){
-            if(0==flag){
-                System.out.println(each_line);
-                header.append(each_line+"\r\n");
-                // between header and body
-                if (each_line.equals(""))
-                    break;
-            }
+            System.out.println(each_line);
+            header.append(each_line+"\r\n");
+            // between header and body
+            if (each_line.equals(""))
+                break;
         }
+        System.out.println("finish parse response header");
+
+
+
+
+        System.out.println("start parse response body");
 
         // body
-        BufferedReader buffer_reader = new BufferedReader(new InputStreamReader(in));
-        StringBuilder body = new StringBuilder();
-        String line = "";
+        //BufferedReader buffer_reader = new BufferedReader(new InputStreamReader(in));
+        //StringBuilder body = new StringBuilder();
+        //String line = "";
+
         int n=0; // sequence
         int size = 1024;
         byte[] bytes=new byte[size]; //store input stram
         int each_byte; // read from input stream
-        while(255!=(each_byte=in.read())) {
-            //DEBUG LINE
-            System.out.println(each_byte);
-
-            if (size == n+1) {
-                byte[] new_bytes = new byte[2 * size];
-                System.arraycopy(bytes, 0, new_bytes, 0, bytes.length);
-                bytes = new_bytes;
-                size*=2;
+        try {
+            while (-1 != (each_byte = in.read())) {
+                //DEBUG LINE
+                System.out.print(each_byte + " ");
+                if (size == n + 1) {
+                    byte[] new_bytes = new byte[2 * size];
+                    System.arraycopy(bytes, 0, new_bytes, 0, bytes.length);
+                    bytes = new_bytes;
+                    size *= 2;
+                }
+                bytes[n++] = (byte) each_byte;
             }
-            bytes[n++] = (byte) each_byte;
-        }
+        }catch(SocketException e){
+            System.out.println("SocketException");
+        }finally {
+            System.out.println("finish parse response body");
+            writeToFile(new String(bytes), local_url);
 
-        writeToFile(new String(bytes), requested_url, local_filename);
+        }
+/*
+        DataInputStream data_input_stream = new DataInputStream(in);
+        DataOutputStream data_output_stream = new DataOutputStream(new FileOutputStream(local_url));
+        byte[] bytes = new byte[1027*9];
+        int len = 0;
+        while((len = data_input_stream.read())!=-1){
+            data_output_stream.write(bytes,0,len);
+        }
+        data_output_stream.flush();
+*/
+
+
+
+        /*
+        OutputStream file_output_stream = new FileOutputStream(local_url);
+        byte[] bytes = new byte[16*1024];
+        int count;
+        while((count = in.read(bytes)) > 0){
+            System.out.print((byte)count);
+            file_output_stream.write((byte)count);
+        }
+        file_output_stream.close();
+        in.close();
+*/
+
+
+
+
 
     }
 
 
-    public static void writeToFile(String body, String requested_url, String local_filename) throws IOException {
-        String filename = "."+requested_url+local_filename;
+    public static void writeToFile(String body, String requested_url) throws IOException {
+        String filename = "."+requested_url;
         OutputStream file = new FileOutputStream(filename);
         System.out.println("start write");
         file.write(body.getBytes());
         file.close();
+        System.out.println("finish write");
     }
 
     public static void main(String[] args){
         //String requested_host="www.baidu.com";
-        String requested_host="120.79.147.165";
+        String requested_host="gaia.cs.umass.edu";
         String proxy_ip="120.79.147.165";
         //String proxy_ip="127.0.0.1";
         int port=9999;
-        String requested_url = "/";
-        String local_filename="baidu.html";
-        Client client=new Client(proxy_ip,port,requested_host,requested_url,local_filename);
+        String requested_url = "/wireshark-labs/HTTP-wireshark-file1.html";
+        Client client=new Client(proxy_ip,port,requested_host,requested_url);
         client.work();
 
     }
--- ProxyServer.java
+++ ProxyServer.java
@@ -4,10 +4,15 @@ import java.net.Socket;
 class ProxyServer{
     final private int WEBPORT=80;
     private Socket client_socket;
-    private String host="";
+    private String host;
+    private String requested_url;
+    private String local_url;
 
     public ProxyServer(ServerSocket listen) throws IOException {
         this.client_socket = listen.accept();
+        host="";
+        requested_url="";
+        local_url="";
     }
 
     public void run() throws IOException {
@@ -22,6 +27,7 @@ class ProxyServer{
         //parseRequest(client_in, host, request);
         String each_line = "";
         LineBuffer line_buffer = new LineBuffer(1024);
+        System.out.println("start parse header");
         while (null != (each_line = line_buffer.readLine(client_in))) {
             //add each line to string
             System.out.println(each_line);
@@ -32,11 +38,20 @@ class ProxyServer{
                 break;
             } else {
                 String[] temp = each_line.split(" ");
+
+                if (temp[0].contains("GET")){
+
+                    String[] requested_url_array = temp[1].split("/");
+                    requested_url = requested_url_array[requested_url_array.length-1];
+
+                }
                 if (temp[0].contains("Host")) {
                     host = temp[1];
                 }
             }
         }
+        System.out.println("finish parse header");
+
 
         Socket proxy_socket = new Socket(host, WEBPORT);
         InputStream proxy_in = proxy_socket.getInputStream();
@@ -44,31 +59,137 @@ class ProxyServer{
         //forward request
         proxy_out.write(request.toString().getBytes());
 
+
+        //parse response and save it local
+        local_url = "../"+requested_url;
+        parseResponse(proxy_in,local_url);
+
         // forward all
-        new ProxyThread(client_in, proxy_out).start();
-        while (true){
-            client_out.write(proxy_in.read());
+        //new ProxyThread(client_in, proxy_out).start();
+        //while (true){
+            //client_out.write(proxy_in.read());
+        //}
+
+        //manually forward
+        File file = new File(local_url);
+        long length =file.length();
+        byte[] bytes = new byte[16*1024];
+        InputStream file_input_stream = new FileInputStream(file);
+        int count;
+        while((count = file_input_stream.read()) > 0){
+            client_out.write((byte)count);
         }
+        client_out.close();
+        client_in.close();
+        client_socket.close();
+
+
+        /*
+        File file = new File(local_url);
+        DataInputStream data_input_stream = new DataInputStream(new BufferedInputStream(new FileInputStream(local_url)));
+        DataOutputStream data_output_stream =new DataOutputStream(new BufferedOutputStream(client_out));
+
+        byte[] buf = new byte[1024*16];
+        int len=0;
+        while((len = data_input_stream.read())!=-1){
+            data_output_stream.write(buf,0,len);
+        }
+        data_output_stream.flush();
+        */
+
+        /*
+        int n=0; // s
+        int size =1024;
+        byte[] bytes=new byte[size]; //store input stram
+        int each_byte; // read from input stream
+        while(-1!=(each_byte=data_input_stream.read())){
+            if (size == n+1) {
+                byte[] new_bytes = new byte[2 * size];
+                System.arraycopy(bytes, 0, new_bytes, 0, bytes.length);
+                bytes = new_bytes;
+                size*=2;
+            }
+
+            bytes[n++] = (byte) each_byte;
+        }
+
+        data_output_stream.write(bytes,0,n);
+        */
+
+
+
+        //data_output_stream.flush();
+        //client_out.flush();
+        System.out.println("finish forward to client");
+        System.out.println("------------");
+
     }
 
-    public static void writeToFile(String response, String requested_url, String local_filename) throws IOException {
-        String filename = "./"+requested_url+"/"+local_filename;
+
+
+    public static void parseResponse(InputStream proxy_in, String local_url) throws IOException {
+        System.out.println("start parse response header");
+        StringBuilder header=new StringBuilder();
+        String each_line="";
+        LineBuffer line_buffer = new LineBuffer(3000);
+        int flag=0;
+        while(null != (each_line = line_buffer.readLine(proxy_in) )){
+            if(0==flag){
+                System.out.println(each_line);
+                header.append(each_line+"\r\n");
+                // between header and body
+                if (each_line.equals(""))
+                    break;
+            }
+        }
+        System.out.println("finish parse response header");
+
+        System.out.println("start parse response body");
+        // body
+        int n=0; // sequence
+        int size = 1024;
+        byte[] bytes=new byte[size]; //store input stram
+        int each_byte; // read from input stream
+        while(-1 != (each_byte=proxy_in.read())) {
+            //DEBUG LINE
+            System.out.print(each_byte+" ");
+            if (size == n+1) {
+                byte[] new_bytes = new byte[2 * size];
+                System.arraycopy(bytes, 0, new_bytes, 0, bytes.length);
+                bytes = new_bytes;
+                size*=2;
+            }
+            bytes[n++] = (byte) each_byte;
+        }
+
+        System.out.println("finish parse response body");
+
+        writeToFile(new String(bytes), local_url);
+
+
+
+    }
+    public static void writeToFile(String body, String local_url) throws IOException {
+        String filename = local_url;
         OutputStream file = new FileOutputStream(filename);
+        System.out.println("start write");
+        file.write(body.getBytes());
+        System.out.println("finish write");
         file.close();
     }
 
-    public static void parseRequest(InputStream in,String host, StringBuilder request) throws IOException {
-        //parse request
 
-    }
+
+
+
 
     public static void main(String[] args) throws IOException{
         int port = 9999;
         ServerSocket server_socket = new ServerSocket(9999);
         ProxyServer proxy_server = new ProxyServer(server_socket);
-        while(true){
-            proxy_server.run();
-        }
+
+        proxy_server.run();
+
     }
 }
 
